e008ecafc4c0b3811aff730682afe25ea83085ca
GPSPic <picard.guillaume47@gmail.com>
2023-04-28 15:05:10 +0100

    install font and style, create posts collection

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,30 +1,47 @@
 use LilyPad;
 db.dropDatabase();
 
-db.frogs.insertMany([
-  {
-    "name": "Kermitt",
-    "image_url": "placeholder",
-    "friends": []
-  },
-  {
-    "name": "Throg",
-    "image_url": "placeholder",
-    "friends": []
-  },
-  {
-    "name": "Bogariel Frogariel",
-    "image_url": "placeholder",
-    "friends": []
-  },
-  {
-    "name": "Hot Toady",
-    "image_url": "placeholder",
-    "friends": []
-  },
-  {
-      "name": "Bully the Frog",
+const frogs = [
+    {
+      "name": "Kermitt",
       "image_url": "placeholder",
       "friends": []
-    }
-])
+    },
+    {
+      "name": "Throg",
+      "image_url": "placeholder",
+      "friends": []
+    },
+    {
+      "name": "Bogariel Frogariel",
+      "image_url": "placeholder",
+      "friends": []
+    },
+    {
+      "name": "Hot Toady",
+      "image_url": "placeholder",
+      "friends": []
+    },
+    {
+        "name": "Bully the Frog",
+        "image_url": "placeholder",
+        "friends": []
+      }
+  ]
+
+const insertFrogResult = db.frogs.insertMany(frogs)
+
+const kermittId = insertFrogResult.insertedIds[0]
+const throgId = insertFrogResult.insertedIds[1]
+
+
+db.posts.insertOne(
+    {
+        "poster": kermittId,
+        "receiver": throgId,
+        "image_url": "https://images.unsplash.com/photo-1622268002940-4a58833ec8f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80",
+        "comment": {
+            "original": ""
+        }
+    },
+)

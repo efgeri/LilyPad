33f738782a7afd7563953eec044fa67d91e84df4
Gergely Farkas <efgeri@gmail.com>
2023-04-28 20:16:03 +0100

    Removed some commented out stuff

--- server/server.js
+++ server/server.js
@@ -27,25 +27,6 @@ MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true})
   })
   .catch(console.error)
 
-// ====== ORIGINAL CONTENT =====
-// MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true})
-//   .then(client => {
-//     const db = client.db('<database_name>');
-//     const NAMECollection = db.collection('<collection_name>')
-//     const NAMERouter = createRouter(NAMECollection)
-//     app.use('/api/NAME', NAMERouter)
-//   })
-//   .catch(console.error)
-
-//   MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true})
-//   .then(client => {
-//     const db = client.db('<database_name>');
-//     const NAMECollection = db.collection('<collection_name>')
-//     const NAMERouter = createRouter(NAMECollection)
-//     app.use('/api/NAME', NAMERouter)
-//   })
-//   .catch(console.error)
-
 app.listen(9000, function () {
   console.log(`Listening on port ${ this.address().port }`);
 });

531d753c28c723358438535762b9ca6e13af296a
Gergely Farkas <efgeri@gmail.com>
2023-04-30 13:05:14 +0100

    Style changes to frog cards

--- client/src/components/FrogList/FrogElement.js
+++ client/src/components/FrogList/FrogElement.js
@@ -8,8 +8,8 @@ const FrogElement = ({ frog, selectFrog }) => {
     selectFrog(frog)
   }
   return (
-    <StyledList>
-      <p onClick={handleSelect}><strong>{frog.name}</strong></p>
+    <StyledList onClick={handleSelect}>
+      <p><strong>{frog.name}</strong></p>
       <StyledImage src={`${frog.image_url}`} alt={`${frog.name}'s profile picture`} />
     </StyledList>
   );
@@ -20,12 +20,14 @@ width: 100%;
 border-radius: 5%;
 `
 const StyledList = styled.li`
+/* flex: 0 1 20; */
 width: 20%;
 font-family: "Raleway", sans-serif;
 text-align: center;
 font-size: 2rem;
 padding: 10px;
-margin: 10px;
+margin: 15px;
+margin-bottom: 25px;
 border-radius: 5%;
 background-color: #C0FFBB;
 p {
@@ -36,6 +38,10 @@ box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   &:hover {
   box-shadow: 0 12px 24px 0 rgba(0,0,0,0.2);
 }
+@media screen and (max-width: 800px) {
+  width: 90%;
+}
+
 `
 
 export default FrogElement;

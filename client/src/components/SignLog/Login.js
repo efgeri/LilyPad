f93bf5c9f268caa3481dfddbe62ece836a9b14df
Gergely Farkas <efgeri@gmail.com>
2023-04-30 21:05:06 +0100

    Logging in will take you to the relevent profile

--- client/src/components/SignLog/Login.js
+++ client/src/components/SignLog/Login.js
@@ -1,11 +1,12 @@
 import FrogList from "../FrogList/FrogList";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Login = ({frogs, selectLoggedinFrog}) => {
   return ( 
     <StyledSection>
       <h2>Login</h2>
-      <FrogList frogs={frogs} selectFrog={selectLoggedinFrog}/>
+      <Link to="/profile"><FrogList frogs={frogs} selectFrog={selectLoggedinFrog}/></Link>
       </StyledSection>
    );
 }
@@ -14,6 +15,12 @@ const StyledSection = styled.section`
 display: flex;
 flex-direction: column;
 align-items: center;
+a {
+  text-decoration: none;
+  &:visited {
+    color: black;
+  }
+}
 `
  
 export default Login;

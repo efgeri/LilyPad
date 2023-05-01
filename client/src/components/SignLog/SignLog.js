06676d9b18401e59c111d70124c102dc4675b0d5
Lewis Brennan <lewisbrennan444@gmail.com>
2023-05-01 23:32:04 +0100

    made css changes to login page

--- client/src/components/SignLog/SignLog.js
+++ client/src/components/SignLog/SignLog.js
@@ -1,5 +1,6 @@
 import FrogForm from "../FrogForm/FrogForm";
 import Login from "./Login";
+import styled from 'styled-components'
 
 const SignLog = ({ addFrog, frogs, selectLoggedinFrog, logOut, loggedFrog, fetchData }) => {
 
@@ -10,10 +11,32 @@ const SignLog = ({ addFrog, frogs, selectLoggedinFrog, logOut, loggedFrog, fetch
 
   return (
     <>
-      {loggedFrog ? <button onClick={handleLogOut}>Peace out</button> : <FrogForm addFrog={addFrog} fetchData={fetchData}/>}      
+    <StyledDiv>
+      {loggedFrog ? <StyledButton onClick={handleLogOut}>Hop off</StyledButton> : <FrogForm addFrog={addFrog} fetchData={fetchData}/>}      
       <Login frogs={frogs} selectLoggedinFrog={selectLoggedinFrog} />
+      </StyledDiv>
     </>
   );
 };
+
+const StyledDiv = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+margin-top: 1%;
+`
+
+const StyledButton = styled.button`
+    background: white;
+    font-family: "Bungee", cursive;
+    color: black;
+    border: 2px solid black;
+    border-radius: 2.5px;
+
+    &:hover {
+    background: red;
+    cursor: pointer;
+    }
+`
 
 export default SignLog;

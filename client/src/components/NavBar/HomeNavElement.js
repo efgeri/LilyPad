ab1534f38b2d70b5871fce68d2cad976fc1480f4
Lewis Brennan <lewisbrennan444@gmail.com>
2023-05-01 14:57:13 +0100

    hover buttons done

--- client/src/components/NavBar/HomeNavElement.js
+++ client/src/components/NavBar/HomeNavElement.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClover } from '@fortawesome/free-solid-svg-icons';
 
 const HomeNavElement = () => {
+
   return (
     <Link to="/">
       <NavigationBarElement>
@@ -15,6 +16,18 @@ const HomeNavElement = () => {
   );
 };
 
+const bounce = keyframes`
+  0%, 20%, 50%, 80%, 100% {
+    transform: translateY(0);
+  }
+  40% {
+    transform: translateY(-5px);
+  }
+  60% {
+    transform: translateY(-3px);
+  }
+`;
+
 const NavigationBarElement = styled.div`
   color: #fc0000;
   display: flex;
@@ -23,14 +36,21 @@ const NavigationBarElement = styled.div`
   font-size: 20px;
   -webkit-text-stroke-width: 1px;
   -webkit-text-stroke-color: black;
+  padding-top: 3px;
   
   @media (max-width: 768px) {
-padding-top: 1.5px;
+  padding-top: 1.5px;
+
   }
   // Add this media query for non-mobile devices
   @media (min-width: 769px) {
-
   }
+
+  &:hover {
+      color: white;
+      animation: ${bounce} 1s;
+    }
+
 `;
 
 

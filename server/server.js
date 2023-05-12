ccc70ada70d59f756d712b17c97402c28fb254ba
Gergely Farkas <efgeri@gmail.com>
2023-05-13 00:22:19 +0100

    Changing .env variable to work

--- server/server.js
+++ server/server.js
@@ -1,16 +1,49 @@
 const express = require('express');
+require("dotenv").config();
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router');
 const cors = require('cors')
 const mongoose = require('mongoose')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DATABASE_URL
+
 
 // middleware
 app.use(express.json());
 app.use(cors())
 
-// MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true})
-mongoose.connect(process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017')
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+console.log(uri)
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
+
+// mongoose.connect(uri || 'mongodb://127.0.0.1:27017')
+MongoClient.connect(uri, {useUnifiedTopology: true})
   .then(client => {
     const db = client.db('LilyPad');
     const frogsCollection = db.collection('frogs')
@@ -19,8 +52,8 @@ mongoose.connect(process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017')
   })
   .catch(console.error)
 
-  // MongoClient.connect('mongodb://127.0.0.1:27017', {useUnifiedTopology: true})
-  mongoose.connect(process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017')
+  // mongoose.connect(uri || 'mongodb://127.0.0.1:27017')
+  MongoClient.connect(uri, {useUnifiedTopology: true})
   .then(client => {
     const db = client.db('LilyPad');
     const postsCollection = db.collection('posts')
